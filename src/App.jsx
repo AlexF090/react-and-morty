f4d8f2683813c2219cf2e4456f3beba5bb0ffce6
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-04-05 11:36:55 +0200

    move components files in components folder, import the components in App.jsx; remove 'temp-files' folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import './App.css';
+import Header from './components/header';
+import Card from './components/card';
+import Navbar from './components/navbar'
 
 function App() {
   return (
     <>
-      <header>
-        <h1>React and Morty</h1>
-      </header>
-      <article>
-        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Profile Pic" />
-        <h2>Morty Smith</h2>
-      </article>
-      <footer>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Placeholder1</li>
-            <li>Placeholder2</li>
-            <li>Placeholder3</li>
-          </ul>
-        </nav>
-      </footer>
+<Header />
+<Card />
+<Navbar />
     </>
   );
 }
