bef89791d6984e29b70cb334220d37a5a8c352c8
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-04-05 14:22:00 +0200

    add the basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import './App.css';
 import Header from './components/header';
 import Card from './components/card';
-import Navbar from './components/navbar'
+import Navbar from './components/navbar';
+import styled from 'styled-components';
+import GlobalStyle from './style/globalStyles.jsx';
 
 function App() {
   return (
     <>
-<Header />
-<Card />
-<Navbar />
+      <GlobalStyle />
+      <Header />
+      <Main>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Main>
+      <Navbar />
     </>
   );
 }
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5em;
+`;
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,38 @@
+import styled from 'styled-components';
+
 const Navbar = () => {
   return (
-    <footer>
+    <FooterWrapper>
       <nav>
-        <ul>
+        <hr />
+        <NavWrapper>
           <li>Home</li>
           <li>Placeholder1</li>
           <li>Placeholder2</li>
           <li>Placeholder3</li>
-        </ul>
+        </NavWrapper>
       </nav>
-    </footer>
+    </FooterWrapper>
   );
 };
+
+const FooterWrapper = styled.footer`
+  position: sticky;
+  bottom: 0;
+  margin-top: 0.5rem;
+`;
+
+const NavWrapper = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  gap: 0.25rem;
+  width: 100vw;
+  background-color: #9ef01a;
+  color: #004B23;
+  text-shadow: 2px 2px 5px #00000069;
+  list-style-type: none;
+  text-decoration: none;
+`;
 
 export default Navbar;
