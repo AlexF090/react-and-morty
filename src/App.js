783d00d23304f750b9fa80b39455c5725d21b545
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-04-05 11:21:10 +0200

    replace the existing components and copy the content of the components in the App.jsx - rename App.js and index.js in '.jsx'

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <header>
+        <h1>React and Morty</h1>
+      </header>
+      <article>
+        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Profile Pic" />
+        <h2>Morty Smith</h2>
+      </article>
+      <footer>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Placeholder1</li>
+            <li>Placeholder2</li>
+            <li>Placeholder3</li>
+          </ul>
+        </nav>
+      </footer>
+    </>
   );
 }
 
